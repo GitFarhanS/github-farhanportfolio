fd1b9380da55362081a2679f1111aaaceb3486b5
psyfs3 <psyfs3@exmail.nottingham.ac.uk>
2024-09-13 16:35:01 +0100

    header

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
 
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-10">
-      <nav className="flex gap-1 p-0.5 border border-white/15 bg-white/10 rounded-full backdrop-blur">
+      <nav className="flex sm:gap-0.5 md:gap-1 p-0.5 border border-white/15 bg-white/10 rounded-full backdrop-blur">
         <ScrollLink sectionId="home" as="button" className={`nav-item ${currentSection === "home" ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "hover:bg-white/10"}`}>
           Home
         </ScrollLink>
