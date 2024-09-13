92cc8a1a1f7e1e3164c86c82d648a9e9ebea0c4c
psyfs3 <psyfs3@exmail.nottingham.ac.uk>
2024-09-13 23:43:55 +0100

    header2

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -10,19 +10,19 @@ export const Header = () => {
   return (
     <div className="flex justify-center items-center fixed top-3 w-full z-10">
       <nav className="flex gap-1 p-0.5 border border-white/15 bg-white/10 rounded-full backdrop-blur">
-        <ScrollLink sectionId="home" as="button" className={`nav-item text-sm md:text-base ${currentSection === "home" ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "hover:bg-white/10"}`}>
+        <ScrollLink sectionId="home" as="button" className={`nav-item ${currentSection === "home" ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "hover:bg-white/10"}`}>
           Home
         </ScrollLink>
-        <ScrollLink sectionId="projects" as="button" className={`nav-item text-sm md:text-base ${currentSection === "projects" ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "hover:bg-white/10"}`}>
+        <ScrollLink sectionId="projects" as="button" className={`nav-item ${currentSection === "projects" ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "hover:bg-white/10"}`}>
           Projects
         </ScrollLink>
-        <ScrollLink sectionId="workExperience" as="button" className={`nav-item text-sm md:text-base ${currentSection === "workExperience" ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "hover:bg-white/10"}`}>
+        <ScrollLink sectionId="workExperience" as="button" className={`nav-item ${currentSection === "workExperience" ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "hover:bg-white/10"}`}>
           Work
         </ScrollLink>
-        <ScrollLink sectionId="about" as="button" className={`nav-item text-sm md:text-base ${currentSection === "about" ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "hover:bg-white/10"}`}>
+        <ScrollLink sectionId="about" as="button" className={`nav-item ${currentSection === "about" ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "hover:bg-white/10"}`}>
           About
         </ScrollLink>
-        <ScrollLink sectionId="contact" as="button" className={`nav-item text-sm md:text-base ${currentSection === "contact" ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "hover:bg-white/10"}`}>
+        <ScrollLink sectionId="contact" as="button" className={`nav-item ${currentSection === "contact" ? "bg-white text-gray-900 hover:bg-white/70 hover:text-gray-900" : "hover:bg-white/10"}`}>
           Contact
         </ScrollLink>
       </nav>
