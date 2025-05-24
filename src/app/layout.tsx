c89795a2d765c54a0733d222422b0e3dc278b5be
farhan <your-username@users.noreply.github.com>
2025-05-25 00:40:07 +0100

    change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/ftlogo.png" type="image/png" />
+        <link rel="icon" href="@/public/ftlogo.png" type="image/png" />
       </head>
       <body className={twMerge(inter.variable, calistoga.variable, "bg-gray-900 text-white antialiased font-sans")}>{children}</body>
     </html>
